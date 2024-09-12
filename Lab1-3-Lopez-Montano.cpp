0b681307371efe6c27f5cdcf7702c5e32dfbbb39
Sami <38861758+sam-lopez@users.noreply.github.com>
2024-09-11 19:52:36 -0500

    Add files via upload

---
+++ Lab1-3-Lopez-Montano.cpp
@@ -0,0 +1,15 @@
+//Samantha Lopez
+//It's a mad, mad program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Success\n";
+	cout << "Success";
+	cout << " Success\n\n";
+	cout << "Success\n";
+
+	return 0;
+}
