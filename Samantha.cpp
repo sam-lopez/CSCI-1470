ca9779b21d85b0160bff54e5b65617724b92e200
Sami <38861758+sam-lopez@users.noreply.github.com>
2024-09-11 19:52:59 -0500

    Add files via upload

---
+++ Samantha.cpp
@@ -0,0 +1,11 @@
+//Samantha Lopez-Montano
+//Displays student name and favorite movie
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Samantha Lopez" << endl;
+	cout << "Favorite Movie: The Breakfast Club\n\n";
+}
