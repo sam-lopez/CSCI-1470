b4c2f552857407c0f224d5b044dbe42cb19e3e00
Sami <38861758+sam-lopez@users.noreply.github.com>
2024-09-11 19:53:20 -0500

    Add files via upload

---
+++ Lab1-1-LopezMontano.cpp
@@ -0,0 +1,14 @@
+//Samantha Lopez-Montano
+//Displays student information
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Name: Samantha Lopez-Montano" << endl;
+	cout << "Major: Computer Science" << endl;
+	cout << "Minor: N/A" << endl;
+	cout << "High School Attended: Searcy High School" << endl;
+	cout << "College Classification: Freshman" << endl;
+}
