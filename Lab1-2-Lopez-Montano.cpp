d4ac0a008afaa78c5ae233b324d181c55c058736
Sami <38861758+sam-lopez@users.noreply.github.com>
2024-09-11 19:55:56 -0500

    Add files via upload

---
+++ Lab1-2-Lopez-Montano.cpp
@@ -0,0 +1,13 @@
+//Samantha Lopez-Montano
+//Displays Ben Franklin quote
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Tell me and I forget. Teach  \n" ;
+	cout << "me and I remember. Involve \n";
+	cout << "     me and I learn.\n\n";
+	cout << "    Benjamin Franklin \n";
+}
