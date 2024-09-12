2bf79a3818ce7901aba166e11d556daebfa8508f
Sami <38861758+sam-lopez@users.noreply.github.com>
2024-09-11 20:05:03 -0500

    8-26 file

---
+++ LectureWork/dataTypes.cpp
@@ -0,0 +1,30 @@
+//Samantha Lopez-Montano
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int whole_number = 10;
+	double decimal_number = 7.5;
+	char alpha = 'a';
+	string name = "Samantha Lopez-Montano";
+	bool answer1 = true;
+	bool answer2 = false;
+	bool answer3 = 1;
+	bool answer4 = 0;
+	cout << "Whole Number is " << whole_number << endl;
+	cout << "Decimal Number is " << decimal_number << endl;
+	cout << "Alpha is " << alpha << endl;
+	cout << "Name is " << name << endl;
+	cout << "Answer 1 is " << answer1 << endl;
+	cout << "Answer 2 is " << answer2 << endl;
+	cout << "Answer 3 is " << answer3 << endl;
+	cout << "Answer 4 is " << answer4 << endl;
+
+	return 0;
+}
+
+
+    
